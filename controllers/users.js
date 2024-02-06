33a5b24674965a485948dc6fb27d5990c81d47b4
apurbahasanj <apurbahasanj@gmail.com>
2024-02-07 00:09:53 +0600

    user delete and role update api updated

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
+const { ObjectId } = require("mongodb");
 const usersCollection = require("../models/users");
 
+// handle post user data if it not exists
 const handlePostUsers = async (req, res) => {
   const user = req.body;
   const result = await usersCollection.insertOne(user);
@@ -21,7 +23,47 @@ const handleGetAdmin = async (req, res) => {
   res.send(result);
 };
 
+// get all users from
+const handleGetAllUsers = async (req, res) => {
+  const user = await usersCollection.find().toArray();
+  res.send(user);
+};
+
+// update user role
+const handleUpdateUserRole = async (req, res) => {
+  const userId = req.params.id;
+  const filter = { _id: new ObjectId(userId) };
+
+  const user = await usersCollection.findOne(filter);
+
+  let updatedRole;
+
+  if (user.role === "admin") {
+    updatedRole = "user";
+  } else if (user.role === "user") {
+    updatedRole = "admin";
+  }
+
+  const result = await usersCollection.updateOne(filter, {
+    $set: { role: updatedRole },
+  });
+  // console.log(user);
+  res.send(result);
+};
+
+// delete user from collection
+const handleDeleteUser = async (req, res) => {
+  const userId = req.params.id;
+  console.log(userId);
+  const filter = { _id: new ObjectId(userId) };
+  const result = await usersCollection.deleteOne(filter);
+  res.send(result);
+};
+
 module.exports = {
   handleGetAdmin,
+  handleGetAllUsers,
+  handleUpdateUserRole,
   handlePostUsers,
+  handleDeleteUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { handlePostUsers, handleGetAdmin } = require("../controllers/users");
+const {
+  handlePostUsers,
+  handleGetAdmin,
+  handleGetAllUsers,
+  handleUpdateUserRole,
+  handleDeleteUser,
+} = require("../controllers/users");
 const { verifyJWT } = require("../services/auth");
 
 router.post("/", handlePostUsers);
+router.get("/admin", handleGetAllUsers);
+router.route("/admin/:id").patch(handleUpdateUserRole).delete(handleDeleteUser);
 router.get("/admin/:email", verifyJWT, handleGetAdmin);
 
 module.exports = router;
